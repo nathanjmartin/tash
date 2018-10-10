f70931e0191eeeb8b9b485f62f89928503b64678
Nathan Martin <nathanm9000@gmail.com>
2018-10-10 12:43:27 -0500

    Commit

--- tash.cpp
+++ tash.cpp
@@ -1,22 +1,12 @@
 #include <iostream>
 #include <string>
-#include <vector>
-
+#include <unistd.h>
 
 using namespace std;
-vector<string> commands;
-
-
-//method to add the list of commands to the command vector
-void addCommands() {
-  commands.push_back("exit");
-  commands.push_back("ls");
-}
 
 //main method
 int main() {
   string userInput; //string to hold the user's input
-  addCommands();
 
   while(true) {     //loop to continue prompting for commands
     cout << "uofmsh> "; //printing out to the console
@@ -26,12 +16,7 @@ int main() {
       break;
     }
     if(userInput == "ls") {
-      // code for ls command
-    }
-    if(userInput == "help") {
-      for(int i = 0; i < commands.size(); i++) {
-        cout << commands.at(i) << endl; //prints out all the commands in the commands vector.
-      }
+      pid_t pid = fork();
     }
   }
   return 0;
