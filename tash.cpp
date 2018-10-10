f51732833a7dacef0b3ef2d6d11a5ad17b05959d
Nathan Martin <nathanm9000@gmail.com>
2018-10-10 13:38:59 -0500

    Added comments for clarification

--- tash.cpp
+++ tash.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <vector>
 #include <unistd.h>
-#include<sys/wait.h>
+#include <sys/wait.h>
 
 using namespace std;
 
@@ -15,7 +15,7 @@ int main() {
     getline(cin, userInput,'\n'); //accepting user input to store in userInput
 
     if(userInput == "exit") {   //conditional for the exit command
-      break;
+      break;  //equivalent of setting status to 0 (false) to end the loop
     }
 
     if(userInput == "ls") {
@@ -24,9 +24,8 @@ int main() {
       if (pid == 0){  //in the child process
         execl("/bin/ls", "ls", "-r", "-t", "-l", (char *) 0);
         }
-
-      else if(pid > 0) {
-        wait(NULL);
+      else if(pid > 0) {   //in the parent process
+        wait(NULL);     //waiting for the child process to finish before continuing execution of the parent process
         }
       }
     }
