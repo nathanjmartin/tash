a75fbd81bfb4db57562989ad1931c574b5ab80dc
Nathan Martin <nathanm9000@gmail.com>
2018-10-13 16:11:30 -0500

    Merging

--- tash.cpp
+++ tash.cpp
@@ -18,16 +18,45 @@ int main() {
       break;  //equivalent of setting status to 0 (false) to end the loop
     }
 
+    //code for ls
     if(userInput == "ls") {
       int pid = fork();
 
-      if (pid == 0){  //in the child process
-        execl("/bin/ls", "ls", "-r", "-t", "-l", (char *) 0);
+      if (pid == 0) {  //in the child process
+        execl("/bin/ls", "ls", "-r", "-t", NULL, (char *) 0);
         }
+
       else if(pid > 0) {   //in the parent process
         wait(NULL);     //waiting for the child process to finish before continuing execution of the parent process
         }
       }
+
+      //code for ll
+      if(userInput == "ll") {
+        int pid = fork();
+
+        if (pid == 0) {  //in the child process
+          execl("/bin/ls", "ls", "-r", "-t", "-l", (char *) 0);
+          }
+
+        else if(pid > 0) {   //in the parent process
+          wait(NULL);     //waiting for the child process to finish before continuing execution of the parent process
+          }
+        }
+
+      //code for change directory (doesn't work)
+      if(userInput == "cd") {
+      int pid = fork();
+      if (pid == 0) { //child processes
+        chdir("/bin/usr");
+      }
+      else if(pid > 0) {
+        wait(NULL);
+      }
+      else {
+        cout << "Fork failed. " << endl;
+      }
+    }
     }
   return 0;
 }
